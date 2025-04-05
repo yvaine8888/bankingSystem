e6c35111f99f0d9d45e897848ebd3878ac053262
Yvaine Liew <yvaine8888@gmail.com>
2025-04-04 21:02:04 -0500

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jonat\Documents\GitHub\c2c_smart_age\venv
+set VIRTUAL_ENV=C:\Users\tclie\Documents\GitHub\bankingSystem\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
