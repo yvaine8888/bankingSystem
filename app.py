ad9ea13e99aaa6990a9d6d652fd40af6a0afb780
Yvaine Liew <yvaine8888@gmail.com>
2025-04-05 16:28:09 -0500

    Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+@app.route('/create_account', methods=['GET'])
+def signUp():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.getElementById("#signup").addEventListener("click", function() { 
+    fetch('/create_account');
+    /**
+    .then(response => response.json())
+    .then(data => {
+    console.log(data.result)*/
+});
