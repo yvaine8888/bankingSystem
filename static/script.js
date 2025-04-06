19d3ad8fd5e4f8a74354a22f810598c49b922b30
Yvaine Liew <yvaine8888@gmail.com>
2025-04-05 22:07:03 -0500

    Update

--- static/script.js
+++ static/script.js
@@ -1,13 +1,8 @@
-document.getElementById("#signup").addEventListener("click", function() { 
-    show('createPage', 'loginPage');
-});
+document.getElementById("#signup").addEventListener("click", show('createPage', 'loginPage'));
 
-document.getElementById("#back").addEventListener("click", function() { 
-    show('loginPage', 'createPage');
-});
+document.getElementById("#back").addEventListener("click", show('loginPage', 'createPage'));
 
-function show(show, hide) {
-    document.getElementById(show).style.display='block';
+function show(showing, hide) {
+    document.getElementById(showing).style.display='block';
     document.getElementById(hide).style.display='none';
-    return false;
 }
